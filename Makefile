982bb0bbcdb62abcb8e86464d64a27f4e9a15f0a
ThreeDP <davy.paulino@aluno.ifsp.edu.br>
2022-12-17 16:59:07 -0300

    Correct the error in the makefile and minunit.h

--- Makefile
+++ Makefile
@@ -1,53 +1,44 @@
 ################  COLORS	##################
-L_RED			=			'\033[0;31m'
-L_REDB			=			'\033[1;31m'
-L_WHITE			=			'\033[0;37m'
-L_WHITEB		=			'\033[1;37m'
-L_YELLOW		=			'\033[0;33m'
-L_YELLOWB		=			'\033[1;33m'
-L_GREEN			=			'\033[0;32m'
-L_GREENB		=			'\033[1;32m'
+L_RED			=			\033[0;31m
+L_REDB			=			\033[1;31m
+L_WHITE			=			\033[0;37m
+L_WHITEB		=			\033[1;37m
+L_YELLOW		=			\033[0;33m
+L_YELLOWB		=			\033[1;33m
+L_GREEN			=			\033[0;32m
+L_GREENB		=			\033[1;32m
 ################ CONFIG		##################
-DIR 			= 			../
-LIB 			= 			libft.a
-# Local do .c
-SRCS 			= 			$(wildcard ./test/*.c)
-HEADERS 		= 			minunit.h test.h
+DIR_OUTS		=			./outs/
+SRCS 			= 			$(wildcard ./*.c)
+RUN				=			$(addprefix $(DIR_OUTS), $(srcs));
+HEADERS_TEST 	= 			minunit.h
 FLAGS 			= 			-Wall -Werror -Wextra
 ################ RULES		##################
 
-all: 		$(SRCS:.c=.o)
+all: 		$(SRCS:.c=.o) $(RUN) clean_outs
 
-%.o: 		%.c $(HEADERS) 
-	@cd 		$(DIR) && $(MAKE)
-	@cc 		-o $@ -c $< $(FLAGS)
-	@gcc		$(FLAGS) $@ -I $(DIR) -L $(DIR) -lft
-	@echo		"$(L_YELLOWB)"RUN TEST: $@ "$(L_WHITE)"
-	@./a.out
-	@sleep .5
+%.o: 		%.c $(HEADERS)
+	@echo  "$(L_YELLOWB)[COMPILE $@]$(L_WHITE)"
+
+$(RUN):
+	@echo  "$(L_YELLOWB)[TEST $@]$(L_WHITE)"
+	@$@
+	@echo  "$(L_WHITEB)"*******************************************************************"$(L_WHITEB)"
 
 norm:
 	@cd 		$(DIR) && $(MAKE) $@
 
-cleann:
-	@rm 		-f tests_libft.o
+clean_outs:
+	@rm 		-f $(DIR_OUTS)
+
+cleant:
 	@rm			-f $(SRCS:.c=.o)
 	
-
-clean:		cleann
+clean:		cleant
 	@cd 		$(DIR) && $(MAKE) $@
 
-
-fclean: 	cleann
+fclean: 	cleant
 	@rm 		-f run.exec
 	@cd 		$(DIR) && $(MAKE) $@
 
-re: fclean all
-
-## Exemplo de test unico de função
-ft_atoi:
-	@cd 		$(DIR) && $(MAKE)
-	@cc 		-o ./test/ft_atoi_test.o -c ./test/ft_atoi_test.c $(FLAGS)
-	@gcc		$(FLAGS) ./test/ft_atoi_test.o -I $(DIR) -L $(DIR) -lft
-	@echo		"$(L_YELLOWB)"RUN TEST: FT_ATOI "$(L_WHITE)"
-	@./a.out
+re: fclean clean clean_outs cleant all
--- minunit.h
+++ minunit.h
@@ -67,12 +67,6 @@
 #include <math.h>
 
 /*  Print with colors */
-	// in case you come across the following error, 
-		// error: ‘%s’ directive output may be truncated writing up to 
-		// 10000 bytes into a region of size 940 [-Werror=format-truncation=]
-	// add the following flag to cc/gcc:
-		// -Wno-format-truncation
-	// as https://github.com/apple/cups/issues/5110
 #define PRINTRED(X)     "\033[31m"X"\033[0m"            /* Red */
 #define PRINTGRN(X)     "\033[32m"X"\033[0m"            /* Green */
 #define BOLDRED(X)      "\033[1m\033[31m"X"\033[0m"     /* Bold Red */
@@ -84,7 +78,6 @@
 #define MINUNIT_EPSILON 1E-12
 
 /*  Misc. counters */
-static int minunit_suite = 0;
 static int minunit_run = 0;
 static int minunit_assert = 0;
 static int minunit_fail = 0;
@@ -111,13 +104,9 @@ static void (*minunit_teardown)(void) = NULL;
 
 /*  Run test suite and unset setup and teardown functions */
 #define MU_RUN_SUITE(suite_name) MU__SAFE_BLOCK(\
-	minunit_suite++;\
-	printf("Test Suite %d:\n", minunit_suite);\
 	suite_name();\
 	minunit_setup = NULL;\
 	minunit_teardown = NULL;\
-	printf("\n\n");\
-
 )
 
 /*  Configure setup and teardown functions */
@@ -134,13 +123,11 @@ static void (*minunit_teardown)(void) = NULL;
 	}\
 	if (minunit_setup) (*minunit_setup)();\
 	minunit_status = 0;\
-
-	printf("\n%d:", minunit_run + 1);\
 	test();\
 	minunit_run++;\
 	if (minunit_status) {\
 		minunit_fail++;\
-		printf(BOLDRED(" KO! "));\
+		printf(BOLDRED("F"));\
 		printf("\n%s\n", minunit_last_message);\
 	}\
 	fflush(stdout);\
@@ -151,7 +138,6 @@ static void (*minunit_teardown)(void) = NULL;
 #define MU_REPORT() MU__SAFE_BLOCK(\
 	double minunit_end_real_timer;\
 	double minunit_end_proc_timer;\
-
     if(minunit_fail)\
     printf(PRINTRED("\n\n%d tests, %d assertions, %d failures\n"), minunit_run, minunit_assert, minunit_fail);\
     else\
@@ -168,17 +154,11 @@ static void (*minunit_teardown)(void) = NULL;
 #define mu_check(test) MU__SAFE_BLOCK(\
 	minunit_assert++;\
 	if (!(test)) {\
-		snprintf(minunit_last_message, MINUNIT_MESSAGE_LEN, "%s failed:\n\t%s:%d: %s", __func__, __FILE__, __LINE__, #test);\
-		minunit_status = 1;\
-		return;\
-	} else {\
-		printf(".");\
-
 		snprintf(minunit_last_message, MINUNIT_MESSAGE_LEN, PRINTRED("%s failed:\n\t%s:%d: %s"), __func__, __FILE__, __LINE__, #test);\
 		minunit_status = 1;\
 		return;\
 	} else {\
-		printf(BOLDGREEN(" OK "));\
+		printf(BOLDGREEN("."));\
 	}\
 )
 
@@ -196,7 +176,7 @@ static void (*minunit_teardown)(void) = NULL;
 		minunit_status = 1;\
 		return;\
 	} else {\
-		printf(BOLDGREEN(" OK "));\
+		printf(BOLDGREEN("."));\
 	}\
 )
 
@@ -211,7 +191,7 @@ static void (*minunit_teardown)(void) = NULL;
 		minunit_status = 1;\
 		return;\
 	} else {\
-		printf(BOLDGREEN(" OK "));\
+		printf(BOLDGREEN("."));\
 	}\
 )
 
@@ -227,7 +207,7 @@ static void (*minunit_teardown)(void) = NULL;
 		minunit_status = 1;\
 		return;\
 	} else {\
-		printf(BOLDGREEN(" OK "));\
+		printf(BOLDGREEN("."));\
 	}\
 )
 
@@ -244,11 +224,12 @@ static void (*minunit_teardown)(void) = NULL;
 	if(strcmp(minunit_tmp_e, minunit_tmp_r)) {\
 		snprintf(minunit_last_message, MINUNIT_MESSAGE_LEN, PRINTRED("%s failed:\n\t%s:%d: '%s' expected but was '%s'"), __func__, __FILE__, __LINE__, minunit_tmp_e, minunit_tmp_r);\
 		minunit_status = 1;\
-		return;}\
-	else {\
-		printf(BOLDGREEN(" OK "));\
+		return;\
+	} else {\
+		printf(BOLDGREEN("."));\
 	}\
 )
+
 /*
  * The following two functions were written by David Robert Nadeau
  * from http://NadeauSoftware.com/ and distributed under the
